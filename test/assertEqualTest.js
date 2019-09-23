ef8eea06b2f80f202203e916ba52b02036d4f097
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-09-23 21:24:39 +0000

    Incorporated mocha and chai for head function with Jordan - Maria driver

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,12 +1,17 @@
 // assertEqual Test
+// required chai
 
+const assert = require('chai').assert;
+/*
 const assertEqual = require('../assertEqual2');
 const tail = require('../tail2');
+*/
 const head = require('../head2');
+/*
 const eqArrays = require('../eqArrays2');
 const assertArraysEqual = require('../assertArraysEqual2');
-
-
+*/
+/*
 // testing tail function with Jordan - Maria driver
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2);
@@ -20,13 +25,21 @@ assertEqual(ingredients.length, 0);
 const list = [];
 tail[list];
 assertEqual(list.length, 0);
-
+*/
 
 // testing head function with Maria - Jordan driver
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// added describe and it functions with Jordan - Maria driver
+describe("#head", () => {
+  it('returns 5 for [5, 6, 7]', () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+  it('returns "Hello" for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});
 
 
+/*
 // testing eqArrays function with Maria - Jordan driver
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); 
@@ -36,3 +49,4 @@ assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
 // testing assertArraysEqual function with Jordan - Maria driver
 assertArraysEqual([1, 2, 3,], [1, 2, 3]);
 assertArraysEqual([1, 2, 3,], [3, 2, 1]);
+*/
