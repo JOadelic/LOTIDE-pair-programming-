51266afcd027b6b746fa3b38fa174e478541267a
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-09-23 20:05:54 +0000

    Added assertArraysEqual function in test file with Jordan - Maria driver

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,12 +1,13 @@
-// Refactoring tail function with Jordan - Maria driver
+// assertEqual Test
 
 const assertEqual = require('../assertEqual2');
 const tail = require('../tail2');
 const head = require('../head2');
 const eqArrays = require('../eqArrays2');
+const assertArraysEqual = require('../assertArraysEqual2');
 
 
-// testing tail function
+// testing tail function with Jordan - Maria driver
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2);
 assertEqual(result[0], "Lighthouse");
@@ -21,12 +22,17 @@ tail[list];
 assertEqual(list.length, 0);
 
 
-// testing head function
+// testing head function with Maria - Jordan driver
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
 
-// testing eqArrays function
+// testing eqArrays function with Maria - Jordan driver
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); 
+
+
+// testing assertArraysEqual function with Jordan - Maria driver
+assertArraysEqual([1, 2, 3,], [1, 2, 3]);
+assertArraysEqual([1, 2, 3,], [3, 2, 1]);
