45f37fae894050dd67d4bc54e559bc4781dbe851
Your Name <you@example.com>
2019-09-23 19:51:50 +0000

    removed test cases with Maria - Jordan is driver

--- tail2.js
+++ tail2.js
@@ -7,21 +7,4 @@ const tail = function(arr) {
   return arr;
 };
 
-// const result = tail(["Hello", "Lighthouse", "Labs"]);
-// assertEqual(result.length, 2);
-// assertEqual(result[0], "Lighthouse");
-// assertEqual(result[1], "Labs");
-// 
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// tail(words);
-// assertEqual(words.length, 3);
-// 
-// const ingredients = ["cheese"];
-// tail(ingredients);
-// assertEqual(ingredients.length, 0);
-// 
-// const list = [];
-// tail[list];
-// assertEqual(list.length, 0);
-
 module.exports = tail;
