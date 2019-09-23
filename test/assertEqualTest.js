804f4eb72e9014a2a31bde23f34226184e69d570
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-09-23 19:25:41 +0000

    Merge

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,10 +1,23 @@
+// Refactoring tail function with Jordan - Maria driver
+
 const assertEqual = require('../assertEqual2');
+const tail = require('../tail2');
 const head = require('../head2');
 
-// TEST CODE
-// assertEqual("Lighthouse Labs", "Lighthouse Labs");
-// assertEqual(5, 1);
-// assertEqual("a", "a");
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+
+const ingredients = ["cheese"];
+tail(ingredients);
+assertEqual(ingredients.length, 0);
+
+
+const list = [];
+tail[list];
+assertEqual(list.length, 0);
+
 
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
