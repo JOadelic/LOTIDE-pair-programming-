4830f870b28309ebccc2b5b52e2c2879d94646b6
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-09-23 22:14:12 +0000

    created describe and it functions for middle function and required chai with Jordan - Maria driver

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,10 +1,20 @@
 // assertArraysEqual Test
+// required chai
 
-const assertArraysEqual = require('../assertArraysEqual2');
+const assert = require('chai').assert;
+//const assertArraysEqual = require('../assertArraysEqual2');
 
 const middle = require('../middle2');
 
 // testing middle function with Maria - Jordan is driver
-assertArraysEqual(middle([1, 2, 3, 5, 6]), [3]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+// added describe and it functions with Jordan - Maria driver
+describe("#middle", () => {
+  it('returns [3] for [1, 2, 3, 5, 6]', () => {
+    assert.deepEqual(middle([1, 2, 3, 5, 6]), [3]);
+  });
+  it('returns [2, 3] for [1, 2, 3, 4]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+});
+
+
