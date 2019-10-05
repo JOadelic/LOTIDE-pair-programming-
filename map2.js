06ce7244b49301cae426c3cc93f2b4ada211ca42
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-10-05 02:31:13 +0000

    Refactored map function = pair programming with Jordan

--- map2.js
+++ map2.js
@@ -1,9 +1,9 @@
 const eqArrays = require('./eqArrays2');
 const assertArraysEqual = require('./assertArraysEqual2');
 
-const map = function(array, callback) {
+const map = function(array) {
   const results = [];
-  for (let item of array) { // loop through the each value in the array
+  for (let item of array) { 
     results.push((item[0]));
 
   }
