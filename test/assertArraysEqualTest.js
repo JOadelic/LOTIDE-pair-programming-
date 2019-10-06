da344a74a1cbeaeddcc2699aa383997822d81d52
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-10-06 01:52:51 +0000

    Added new describe and it functions for new refactored functions - pair programming

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -5,6 +5,8 @@ const assert = require('chai').assert;
 //const assertArraysEqual = require('../assertArraysEqual2');
 
 const middle = require('../middle2');
+const map = require('../map2');
+const takeUntil = require('../takeUntil2');
 
 // testing middle function with Maria - Jordan is driver
 // added describe and it functions with Jordan - Maria driver
@@ -14,6 +16,20 @@ describe("#middle", () => {
   });
   it('returns [2, 3] for [1, 2, 3, 4]', () => {
     assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+});
+
+// testing map function
+describe("#map", () => {
+  it('returns ["g", "c", "t", "m", "t"] for ["ground", "control", "to", "major", "tom"]', () => {
+    assert.deepEqual(map(["ground", "control", "to", "major", "tom"]), ["g", "c", "t", "m", "t"]);
+  });
+});
+
+// testing takeUntil function
+describe("#takeUntil", () => {
+  it('returns [ 1, 2, 5, 7, 2 ] for [1, 2, 5, 7, 2, -1, 2, 4, 5]', () => {
+    assert.deepEqual(takeUntil([1, 2, 5, 7, 2, -1, 2, 4, 5], x => x < 0), [ 1, 2, 5, 7, 2 ]);
   });
 });
 
--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -5,6 +5,10 @@ const assert = require('chai').assert;
 
 const tail = require('../tail2');
 const head = require('../head2');
+const findKeyByValue = require('../findKeyByValue2');
+const findKeys = require('../findKeys2');
+const eqObjects = require('../eqObjects2');
+const countOnly = require('../countOnly2');
 
 // testing tail function with Jordan - Maria driver
 // added describe function with Maria - Jordan is driver
@@ -25,5 +29,41 @@ describe("#head", () => {
   });
   it('returns "Hello" for ["Hello", "Lighthouse", "Labs"]', () => {
     assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});
+
+// testing findKeyByValue function
+describe("#findKeyByValue", () => {
+  const bestTVShowsByGenre = {
+    sci_fi: "The Expanse",
+    comedy: "Brooklyn Nine-Nine",
+    drama: "The Wire"
+  };
+  it('returns "drama" for (bestTVShowsByGenre, "The Wire")', () => {
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+  });
+});
+
+// testing findKeys function
+describe("#findKeys", () => {
+  const obj = {
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  }
+  it('returns "Akaleri" for obj', () => {
+    assert.strictEqual(findKeys(obj, x => x.stars === 3), "Akaleri");
+  });
+});
+
+// testing eqObjects function
+describe("#eqObjects", () => {
+  let arrayObject2 = { c: "1", d: ["2", 3] };
+  let arrayObject = { c: "1", d: ["2", 3, 4, 5] };
+  it('returns "false" for (arrayObject2, arrayObject)', () => {
+    assert.strictEqual(eqObjects(arrayObject2, arrayObject), false);
   });
 });
