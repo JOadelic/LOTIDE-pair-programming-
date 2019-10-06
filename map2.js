c660a534eebdfadb6ec3b8ff2d92cfc992a61b0d
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-10-06 02:26:05 +0000

    Modified refactored map function - pair programming

--- map2.js
+++ map2.js
@@ -1,10 +1,10 @@
 const eqArrays = require('./eqArrays2');
 const assertArraysEqual = require('./assertArraysEqual2');
 
-const map = function(array) {
+const map = function(array, callback) {
   const results = [];
   for (let item of array) { 
-    results.push((item[0]));
+    results.push(callback(item));
 
   }
   return results;
